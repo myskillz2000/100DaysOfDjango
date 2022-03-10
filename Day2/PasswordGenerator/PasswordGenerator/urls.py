3e741223810315601615720a64200ed0639d1d0e
Matthew Stiller <30756911+myskillz2000@users.noreply.github.com>
2022-03-10 18:29:56 -0500

    Modified my urls file.

--- Day2/PasswordGenerator/PasswordGenerator/urls.py
+++ Day2/PasswordGenerator/PasswordGenerator/urls.py
@@ -19,5 +19,7 @@ from passwordGen import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path("password/", views.password, name='password'),
+    path("about/", views.about, name='about')
 ]
