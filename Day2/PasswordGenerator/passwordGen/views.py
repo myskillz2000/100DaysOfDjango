828b12d12937c27ce0120354e582b5c08bc95d9e
Matthew Stiller <30756911+myskillz2000@users.noreply.github.com>
2022-03-10 18:27:46 -0500

    Modified the views file.

--- Day2/PasswordGenerator/passwordGen/views.py
+++ Day2/PasswordGenerator/passwordGen/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
     return render(request, 'passwordGen/home.html')
+
+def password(request):
+
+    characters = list()
+    if request.GET.get('lowercase'):
+        characters.extend(list('abcdefghijklmnopqrstuvwxyz'))
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('~!@#$%^&*()_+'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    length = int(request.GET.get('length',13))
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'passwordGen/password.html', {'password':thepassword})
+
+def about(request):
+    return render(request, 'passwordGen/about.html')
